1eb827abf02c5dee0a99c4894ed157d457f28def
unknown <quocbao.job106204@gmail.com>
2025-12-02 04:53:30 +0700

    error: check

--- DoAnCaNhan/Default.aspx.cs
+++ DoAnCaNhan/Default.aspx.cs
@@ -1,17 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
-namespace DoAnCaNhan
+public partial class Default : System.Web.UI.Page
 {
-    public partial class Default : System.Web.UI.Page
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
+        if (!IsPostBack)
         {
+            LoadHeroProducts();
+            LoadBestSellerROG();
+        }
+    }
 
+    private void LoadHeroProducts()
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["ShopPCConnection"].ConnectionString;
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+            string sql = @"SELECT TOP 3 ProductId, ProductName, Price, ImageUrl FROM Products ORDER BY ProductId";
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            rptHero.DataSource = dt;
+            rptHero.DataBind();
+        }
+    }
+
+    private void LoadBestSellerROG()
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["ShopPCConnection"].ConnectionString;
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+            string sql = @"SELECT TOP 12 ProductId, ProductName, Price, ImageUrl FROM Products WHERE Brand='ASUS ROG' ORDER BY ProductId";
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            rptBestSeller.DataSource = dt;
+            rptBestSeller.DataBind();
         }
     }
 }
